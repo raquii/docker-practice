836b26b3d8a5b7b989bf59da810e14c4683f6a58
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2022-02-18 10:42:04 -0500

    fix missing env specification

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
 require 'bundler/setup'
+env = (ENV['RACK_ENV'] || :development)
 
-Bundler.require :default
+Bundler.require :default, env.to_sym
 
 OTR::ActiveRecord.configure_from_file! "./config/database.yml"
 OTR::ActiveRecord.establish_connection!
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 ActiveSupport::Inflector.inflections do |inflect|
     inflect.irregular 'human', 'humans'
