163207cf752ed8407c2bd772d5b44ad8f9ed33bb
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2022-02-22 16:50:20 -0500

    mixin fav food module

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,6 @@
+require_relative '../modules/favorite_food'
 class Dog < ActiveRecord::Base
+    include FavoriteFood
+    
     belongs_to :human
 end
--- app/models/human.rb
+++ app/models/human.rb
@@ -1,3 +1,6 @@
+require_relative '../modules/favorite_food'
 class Human < ActiveRecord::Base
+    include FavoriteFood
+    
     has_many :dogs
 end
