f652d41e5f164333567e5be393cbdfeb9011db56
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2022-02-22 17:45:09 -0500

    add create and show functionality for human

--- api/humans.rb
+++ api/humans.rb
@@ -2,10 +2,50 @@ module API
     class Humans < Grape::API
         format :json
 
+
+        helpers do
+            def human_params(params)
+              {
+                name: params[:name],
+                favorite_color: params[:favorite_color],
+                favorite_food: params[:favorite_food],
+                city: params[:city],
+                date_of_birth: params[:date_of_birth]
+              }
+            end
+        end
+
         resource :human do
+            desc 'Returns all humans'
             get do
                 Human.all
             end
+
+            desc 'Returns a human'
+            params do 
+                requires :id, type: Integer
+            end
+            route_param :id do
+                get do
+                  Human.find(params[:id])
+                end
+            end
+
+            desc 'Creates a human'
+            params do
+                requires :name, type: String
+                optional :favorite_food, type: String
+                optional :favorite_color, type: String
+                optional :date_of_birth, type: Date
+                optional :city, type: String
+            end
+            post do
+                human = Human.new(human_params(params))
+                human.save!
+
+                human
+            end
+
         end
     end
     
