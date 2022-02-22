0eccef7e36acabca85eb9ec5291af96c5f6db541
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2022-02-22 15:18:24 -0500

    remove incorrect desc resource path

--- api/humans.rb
+++ api/humans.rb
@@ -2,7 +2,6 @@ module API
     class Humans < Grape::API
         format :json
 
-        desc 'Mirrors the received string message'
         resource :human do
             get do
                 Human.all
