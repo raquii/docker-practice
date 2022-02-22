f7e7decc6a9cab8774365b5d15cc9e3a484a8b30
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2022-02-22 15:17:35 -0500

    config db from url for local vs docker use

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'bundler/setup'
-env = (ENV['RACK_ENV'] || :development)
 
-Bundler.require :default, env.to_sym
+env = ENV['RACK_ENV'].to_sym || :development
 
-OTR::ActiveRecord.configure_from_file! "./config/database.yml"
+Bundler.require(:default, env)
+
+OTR::ActiveRecord.configure_from_url! ENV['DATABASE_URL']
 OTR::ActiveRecord.establish_connection!
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
